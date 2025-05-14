28ed87a1cce044d8ddcf75fb206b938060c572a9
Uzair390-Del <uzair.zairi321@gmail.com>
2025-05-14 15:29:59 +0500

    makingfragments

--- app/src/main/java/com/example/sendingdatafromactivitytofragment/MainActivity.java
+++ app/src/main/java/com/example/sendingdatafromactivitytofragment/MainActivity.java
@@ -1,15 +1,21 @@
 package com.example.sendingdatafromactivitytofragment;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText editWeight, editHieght;
+    Button calculate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +24,23 @@ public class MainActivity extends AppCompatActivity {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+           FragmentManager fragmentManager= getSupportFragmentManager();
+           FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+           MyFisrtFragment myFisrtFragment=new MyFisrtFragment();
+            editHieght= findViewById(R.id.editTextWeight);
+            editWeight=findViewById(R.id.editTextHieght);
+            calculate=findViewById(R.id.buttonCalculate);
+            calculate.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Bundle bundle=new Bundle();
+                    int userWeight= Integer.parseInt(editWeight.getText().toString());
+                    int userHieght= Integer.parseInt(editHieght.getText().toString());
+                    bundle.putInt("weight",userWeight);
+                    bundle.putInt("hieght",userHieght);
+                }
+            });
+
             return insets;
         });
     }
